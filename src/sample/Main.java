85a2094cb087cb21615072af068045138eeb2be5
nithyajai <nithyajai23@gmail.com>
2022-04-23 22:17:44 +0200

    FIRST COMMIT

--- src/sample/Main.java
+++ src/sample/Main.java
@@ -1,18 +1,8 @@
 package sample;
 
-import javafx.application.Application;
-import javafx.fxml.FXMLLoader;
-import javafx.scene.Parent;
-import javafx.scene.Scene;
-import javafx.stage.Stage;
-
-import java.io.PrintStream;
-import java.util.stream.Stream;
-
-public class Main  {
-
+public class Main {
 
     public static void main(String[] args) {
-
+        System.out.println("this is first");
     }
 }
